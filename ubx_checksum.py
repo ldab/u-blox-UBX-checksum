42745c429b3029a2b69d80dbcd6dbb6ee1ae8855
ldab <34199302+ldab@users.noreply.github.com>
2019-02-20 14:41:25 +0100

    Create ubx_checksum.py

---
+++ ubx_checksum.py
@@ -0,0 +1,17 @@
+
+# CFG-RATE (0x06,0x08) packet (without header 0xB5,0x62)
+# payload length - 6 (little endian format), update rate 200ms, cycles - 1, reference - UTC (0)
+packet = []
+
+packet = input('What is the payload? ')
+
+CK_A,CK_B = 0, 0
+for i in range(len(packet)):
+  CK_A = CK_A + packet[i]
+  CK_B = CK_B + CK_A
+
+# ensure unsigned byte range
+CK_A = CK_A & 0xFF
+CK_B = CK_B & 0xFF
+
+print "UBX packet checksum:", ("0x%02X,0x%02X" % (CK_A,CK_B))
