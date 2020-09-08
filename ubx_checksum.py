dc139d234c8d859018e8fef8d85fb24b71848e1e
Leonardo Bispo <34199302+ldab@users.noreply.github.com>
2020-09-08 15:23:36 +0200

    Update ubx_checksum.py

--- ubx_checksum.py
+++ ubx_checksum.py
@@ -14,4 +14,4 @@ for i in range(len(packet)):
 CK_A = CK_A & 0xFF
 CK_B = CK_B & 0xFF
 
-print "UBX packet checksum:", ("0x%02X,0x%02X" % (CK_A,CK_B))
+print ("UBX packet checksum:", ("0x%02X,0x%02X" % (CK_A,CK_B)))
